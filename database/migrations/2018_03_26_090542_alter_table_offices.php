68405fdc590c5474581338fd85147f1bdb6e430d
Narciso Batingal <batingalnarz11@gmail.com>
2018-04-06 09:10:37 +0800

    updated migration

--- database/migrations/2018_03_26_090542_alter_table_offices.php
+++ database/migrations/2018_03_26_090542_alter_table_offices.php
@@ -14,9 +14,9 @@ class AlterTableOffices extends Migration
     public function up()
     {
         Schema::table('offices', function (Blueprint $table) {
-            $table->integer('parent_office_id')->unsigned()->nullable()->after('acronym');
+            $table->integer('head_office_id')->unsigned()->nullable()->after('acronym');
 
-            $table->foreign('parent_office_id')->references('id')->on('offices')->onUpdate('cascade')->onDelete('set null');
+            $table->foreign('head_office_id')->references('id')->on('offices')->onUpdate('cascade')->onDelete('set null');
         });
     }
 
@@ -28,7 +28,7 @@ class AlterTableOffices extends Migration
     public function down()
     {
         Schema::table('offices', function (Blueprint $table) {
-            $table->dropForeign(['parent_office_id']);
+            $table->dropForeign(['head_office_id']);
         });
     }
 }
