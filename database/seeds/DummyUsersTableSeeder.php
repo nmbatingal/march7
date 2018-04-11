ec983cb1e74893128d4233d9fc6c684b9f701e67
Narciso Batingal <batingalnarz11@gmail.com>
2018-04-11 10:07:40 +0800

    updated

---
+++ database/seeds/DummyUsersTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\User;
+use App\Offices;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Seeder;
+
+class DummyUsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	// $office = Offices::where('acronym', '=', 'TSS')->firstOrFail();
+        $roles  = Role::where('name', 'System Administrator')->firstOrFail();
+
+        $users = [
+            [
+                'username'   => 'admin',
+                'password'   => bcrypt('dostcaraga'),
+                'lastname'   => 'account',
+                'firstname'  => 'admin',
+                'sex'	     => 0,
+                'birthday'   => date("Y-m-d"),
+                'email'	     => 'admindost@caraga.com',
+                // 'office_id'  => $office->id,
+                'position'   => 'System Administrator',
+                '_isActive'  => 1,
+                '_isAdmin'   => 1,
+            ],
+        ];
+
+        foreach ($users as $user) {
+            $u = User::create($user);
+            $u->roles()->sync($roles);
+        }
+
+    }
+}
